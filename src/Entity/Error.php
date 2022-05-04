dc5e490abfa224ed51f6d9b1d3e15ec5e5c0faf1
donSantis <santisamaya@gmail.com>
2022-05-04 03:03:01 -0500

    prueba

---
+++ src/Entity/Error.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+    class Error
+    {
+        public int $id_error;
+        public string $rutine;
+        public string $error;
+        public string $publication_date;
+        public string $modification_date;
+    }
+
+
+
+
+
+
+
+
