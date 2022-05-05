6d55b96133e080bce05887fdb9e7db78fc462b69
Diego <diegoiretamalp@gmail.com>
2022-05-04 21:29:29 -0400

    msg

--- config/routes.php
+++ config/routes.php
@@ -3,10 +3,11 @@
 use League\Route\Router;
 use League\Route\RouteGroup;
 use App\Controller\AdminController;
+use App\Controller\LandingController;
 
 //Controlador de las rutas Esta nos permitiran redirigirnos a los diferentes clases de nuestro proyecto
 return function (Router $router): void {
 
-    $router->get('/', [\App\Controller\LandingController::class, 'index']);
+    $router->get('/', [LandingController::class, 'index']);
 
 };
